6c3ab732662faa48ba462ed919cefadcfd0d77b1
Jiho Han <mlpzaq123456@naver.com>
2021-05-13 17:13:09 +0900

    13.7 AppLoading 2

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
@@ -11,12 +12,17 @@ export default function App() {
   const preload = () => {
     const fontsToLoad = [Iconicons.font];
     const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    return Promise.all(fontPromises);
+    const imageToLoad = [
+      require("./assets/icon.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
 
   if (loading) {
     return (
-      <Apploading
+      <AppLoading
         startAsync={preload}
         onError={console.warn}
         onFinish={onFinish}
