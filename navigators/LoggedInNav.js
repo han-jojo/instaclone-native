917e8a6306c4aa86ddd7d78e8c7180751b5c01bf
Jiho Han <mlpzaq123456@naver.com>
2021-05-20 13:33:04 +0900

    15.0 Navigator 1

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Iconicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Feed from "../screen/Feed";
 
@@ -6,8 +7,52 @@ const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: "white",
+        showLabel: false,
+        style: {
+          borderTopColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Iconicons name="home" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Iconicons name="search" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Iconicons name="heart" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Iconicons name="person" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
