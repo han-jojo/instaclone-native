9c642d76b2eaaefaf5bde399560a2aa07dc1cd03
Jiho Han <mlpzaq123456@naver.com>
2021-05-23 15:36:22 +0900

    15.5 FlatList

---
+++ components/ScreenLayout.js
@@ -0,0 +1,18 @@
+import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
+import React from "react";
+import { ActivityIndicator, View } from "react-native";
+
+export default function ScreenLayout({ loading, children }) {
+  return (
+    <View
+      style={{
+        backgroundColor: "black",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {loading ? <ActivityIndicator color="white" /> : children}
+    </View>
+  );
+}
