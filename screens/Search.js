461c9abf6c50b32705870f6b6f7cc8a55ec62347
Jiho Han <mlpzaq123456@naver.com>
2021-05-26 18:53:12 +0900

    16.6 Search 4

--- screens/Search.js
+++ screens/Search.js
@@ -1,7 +1,14 @@
 import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 
@@ -14,13 +21,43 @@ const SEARCH_PHOTO = gql`
   }
 `;
 
-const Input = styled.TextInput``;
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
+
+const Input = styled.TextInput`
+  background-color: rgba(255, 255, 255, 1);
+  color: black;
+  width: ${(props) => props.width / 1.5}px;
+  padding: 5px 10px;
+  border-radius: 7px;
+`;
 
 export default function Search({ navigation }) {
-  const { setValue, register } = useForm();
-  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTO);
+  const numColumns = 3;
+  const { width } = useWindowDimensions();
+  const { setValue, register, watch, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTO);
+
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
+
   const SearchBox = () => (
-    <TextInput
+    <Input
+      width={width}
       style={{ backgroundColor: "white" }}
       placeholderTextColor="black"
       placeholder="검색"
@@ -29,6 +66,7 @@ export default function Search({ navigation }) {
       returnKeyType="search"
       autoCorrect={false}
       onChangeText={(text) => setValue("keyword", text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
 
@@ -36,20 +74,49 @@ export default function Search({ navigation }) {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register("keyword");
+    register("keyword", {
+      required: true,
+      minLength: 3,
+    });
   }, []);
+
+  const renderItem = ({ item: photo }) => (
+    <TouchableOpacity>
+      <Image
+        source={{ uri: photo.file }}
+        style={{ width: width / numColumns, height: 150 }}
+      />
+    </TouchableOpacity>
+  );
 
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: "black",
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Search</Text>
+      <View style={{ backgroundColor: "black", flex: 1 }}>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>검색 중...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>키워드 검색</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined ? (
+          data?.searchPhotos.length === 0 ? (
+            <MessageContainer>
+              <MessageText>검색 결과 없음</MessageText>
+            </MessageContainer>
+          ) : (
+            <FlatList
+              numColumns={numColumns}
+              data={data?.searchPhotos}
+              keyExtractor={(photo) => "" + photo.id}
+              renderItem={renderItem}
+            />
+          )
+        ) : null}
       </View>
     </DismissKeyboard>
   );
