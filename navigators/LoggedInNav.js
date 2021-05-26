c648497a6f27d2a4822350c14dce19290afc2b08
Jiho Han <mlpzaq123456@naver.com>
2021-05-26 17:48:41 +0900

    16.4 Search 2

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import TabIcon from "../components/nav/TabIcon";
 import SharedStackNav from "./SharedStackNav";
+import useMe from "../hooks/useMe";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
+  const { data } = useMe();
+
   return (
     <Tabs.Navigator
       tabBarOptions={{
@@ -60,9 +63,20 @@ export default function LoggedInNav() {
       <Tabs.Screen
         name="Me"
         options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"person"} color={color} focused={focused} />
-          ),
+          tabBarIcon: ({ focused, color, size }) =>
+            data?.me?.avatar ? (
+              <Image
+                source={{ uri: data.me.avatar }}
+                style={{
+                  height: 30,
+                  width: 30,
+                  borderRadius: 25,
+                  ...(focused && { borderColor: "white", borderWidth: 2 }),
+                }}
+              />
+            ) : (
+              <TabIcon iconName={"person"} color={color} focused={focused} />
+            ),
         }}
       >
         {() => <SharedStackNav screenName="Me" />}
