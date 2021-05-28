3093f5acf488bc37e578a8ccc41f6efc980c0d29
Jiho Han <mlpzaq123456@naver.com>
2021-05-28 14:43:56 +0900

    17.3 Select Photo 2

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,14 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Ionicons } from "@expo/vector-icons";
 import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
-import {
-  FlatList,
-  Image,
-  TouchableOpacity,
-  useWindowDimensions,
-} from "react-native";
-import { colors } from "../colors";
 
 const Container = styled.View`
   flex: 1;
@@ -25,28 +17,14 @@ const Bottom = styled.View`
   background-color: black;
 `;
 
-const ImageContainer = styled.TouchableOpacity``;
-const IconContainer = styled.View`
-  position: absolute;
-  bottom: 5px;
-  right: 0px;
-`;
-
-const HeaderRightText = styled.Text`
-  color: ${colors.blue};
-  font-size: 16px;
-  font-weight: 600;
-  margin-right: 7px;
-`;
-
-export default function SelectPhoto({ navigation }) {
+export default function SelectPhoto() {
   const [ok, setOk] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [chosenPhoto, setChosenPhoto] = useState("");
   const getPhotos = async () => {
-    const { assets: photos } = await MediaLibrary.getAssetsAsync();
-    setPhotos(photos);
-    setChosenPhoto(photos[0]?.uri);
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      setPhotos(photos);
+    }
   };
   const getPermissions = async () => {
     const {
@@ -57,64 +35,19 @@ export default function SelectPhoto({ navigation }) {
       const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
       if (accessPrivileges !== "none") {
         setOk(true);
-        getPhotos();
       }
     } else if (accessPrivileges !== "none") {
       setOk(true);
-      getPhotos();
     }
   };
-  const HeaderRight = () => (
-    <TouchableOpacity>
-      <HeaderRightText>Next</HeaderRightText>
-    </TouchableOpacity>
-  );
   useEffect(() => {
     getPermissions();
+    getPhotos();
   }, []);
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: HeaderRight,
-    });
-  }, []);
-  const numColumns = 4;
-  const { width } = useWindowDimensions();
-  const choosePhoto = (uri) => {
-    setChosenPhoto(uri);
-  };
-  const renderItem = ({ item: photo }) => (
-    <ImageContainer onPress={() => choosePhoto(photo.uri)}>
-      <Image
-        source={{ uri: photo.uri }}
-        style={{ width: width / numColumns, height: 100 }}
-      />
-      <IconContainer>
-        <Ionicons
-          name="checkmark-circle"
-          size={18}
-          color={photo.uri === chosenPhoto ? colors.blue : "white"}
-        />
-      </IconContainer>
-    </ImageContainer>
-  );
   return (
     <Container>
-      <Top>
-        {chosenPhoto !== "" ? (
-          <Image
-            source={{ uri: chosenPhoto }}
-            style={{ width, height: "100%" }}
-          />
-        ) : null}
-      </Top>
-      <Bottom>
-        <FlatList
-          data={photos}
-          numColumns={numColumns}
-          keyExtractor={(photo) => photo.id}
-          renderItem={renderItem}
-        />
-      </Bottom>
+      <Top />
+      <Bottom></Bottom>
     </Container>
   );
 }
